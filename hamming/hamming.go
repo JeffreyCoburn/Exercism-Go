d9fe069e5ee74515402eafe8da2badc78413d6ec
Jeff <jeff.coburn@colostate.edu>
2021-01-31 17:56:42 -0700

    Added comments

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,9 +1,11 @@
+// Package hamming calculates the Hamming distance between two DNA strand sequences
 package hamming
 
 import "errors"
 
 var ErrDifferentLengths = errors.New("cannot compare DNA strands of different lengths")
 
+// Distance returns the hamming distance between two DNA sequences
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, ErrDifferentLengths
