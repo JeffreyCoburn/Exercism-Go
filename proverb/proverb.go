0ac2002133b526342701634b48155cf63d81c0d3
Jeff <jeff.coburn@colostate.edu>
2021-02-08 19:10:29 -0700

    Changed lines to be const

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,18 +1,28 @@
 // Package proverb takes a slice of words and modifies the proverb "For want of a nail.." to use the provided words
 package proverb
 
-// Proverb takes a slice of words and interjects them into the proverb, "For want of a <...> the <...> was lost." and ends with "And all for the want of a <...>."
+import "fmt"
+
+const (
+	line = "For want of a %s the %s was lost."
+	last = "And all for the want of a %s."
+)
+
+// Proverb takes a slice of words and interjects them into the proverb in the format:
+//   "For want of a <...> the <...> was lost."
+//   ...
+//   "And all for the want of a <...>."
 func Proverb(rhyme []string) []string {
-	var proverb []string = make([]string, len(rhyme))
 	if len(rhyme) == 0 {
 		return []string{}
 	}
-	proverb[len(rhyme)-1] = "And all for the want of a " + rhyme[0] + "."
+	var proverb []string = make([]string, len(rhyme))
+	proverb[len(rhyme)-1] = fmt.Sprintf(last, rhyme[0])
 	if len(rhyme) == 1 {
 		return proverb
 	}
 	for i := 0; i < len(rhyme)-1; i++ {
-		proverb[i] = "For want of a " + rhyme[i] + " the " + rhyme[i+1] + " was lost."
+		proverb[i] = fmt.Sprintf(line, rhyme[i], rhyme[i+1])
 	}
 	return proverb
 }
