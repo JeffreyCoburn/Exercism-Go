acfed5658cdd5fbfc73ca8b6d872ee6ed0ebfd88
Jeff <jeff.coburn@colostate.edu>
2021-09-14 12:06:49 -0600

    Convert string to lowercase outside the loop instead of each character, and moved check if the char is a letter earlier in the loop

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,20 +1,25 @@
 package isogram
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
 
 // IsIsogram checks if a string is na isogram
 func IsIsogram(input string) bool {
 	charsInInput := make(map[rune]int) // Stores the characters of the input string
-	for _, char := range input {
+	s := strings.ToLower(input)
+	for _, char := range s {
+		if !unicode.IsLetter(char) {
+			continue
+		}
 		// Check if character has been used already
-		_, found := charsInInput[unicode.ToLower(char)]
+		_, found := charsInInput[char]
 		if found {
 			return false
 		}
 		// If the character wasn't found, add the character to the map
-		if unicode.IsLetter(char) {
-			charsInInput[unicode.ToLower(char)] = 1
-		}
+		charsInInput[unicode.ToLower(char)] = 1
 	}
 	return true
 
