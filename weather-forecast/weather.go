2b8cb0a35c85d9d1c7ea4c810b36e932042f51ad
Jeff <jeff.coburn@colostate.edu>
2021-09-14 14:08:13 -0600

    Passing all tests

--- weather-forecast/weather.go
+++ weather-forecast/weather.go
@@ -1,10 +1,14 @@
+// Package weather creates log entry strings based on a location and condition
 package weather
 
 var (
+	// CurrentCondition contains the current weather conditions
 	CurrentCondition string
-	CurrentLocation  string
+	// CurrentLocation contains the current location
+	CurrentLocation string
 )
 
+// Log returns a description based on the location and condition passed in
 func Log(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
