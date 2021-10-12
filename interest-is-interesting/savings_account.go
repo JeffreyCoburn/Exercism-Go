95386e3d986ad45b592cb2844ceb3bf188512a70
Jeff <jeff.coburn@colostate.edu>
2021-10-12 15:42:35 -0600

    interest-is-interesting passing all tests

---
+++ interest-is-interesting/savings_account.go
@@ -0,0 +1,42 @@
+package savings
+
+import "math"
+
+// InterestRate calculates the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return float32(-3.213)
+	case balance < 1000:
+		return float32(0.5)
+	case balance < 5000:
+		return float32(1.621)
+	default:
+		return float32(2.475)
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return balance * math.Abs(float64(InterestRate(balance))/100)
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	// Check if balance will never reach target
+	if balance*targetBalance < 0 {
+		return -1
+	}
+	years := 0
+	for math.Abs(balance) <= math.Abs(targetBalance) {
+		years++
+		balance = AnnualBalanceUpdate(balance)
+	}
+	return years
+
+}
